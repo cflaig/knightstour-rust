fead4df670c48add559f7cc3768bd4c5753afcd1
Cyril Flaig <code@flaig.ch>
2025-01-28 21:34:35 +0100

    Add binary heap to improve performance
    Performance improved by 64 - 71%
    
    Benchmark on Intel(R) Core(TM) i7-1260P:
    
    Starting on  6x6
         1 Solutions in    0.305s     3.27 Solutions/s      10998716 Nodes
    On  6x6     9862 Solutions in   14.567s   677.02 Solutions/s
    489277566 Nodes
    
    Starting on  8x8
         1 Solutions in    0.000s 29237.20 Solutions/s            75 Nodes
     32769 Solutions in    1.635s 20042.15 Solutions/s      58123690 Nodes
     65537 Solutions in    3.348s 19575.65 Solutions/s     119434577 Nodes
     98305 Solutions in    4.951s 19854.87 Solutions/s     177221857 Nodes
    131073 Solutions in    6.966s 18815.20 Solutions/s     249424648 Nodes
    163841 Solutions in    9.192s 17823.41 Solutions/s     327749258 Nodes
    196609 Solutions in   11.236s 17498.40 Solutions/s     403107848 Nodes

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use clap::Command;
 const KNIGHT_MOVES: [(i8, i8); 8] = [(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)];
 
 #[allow(dead_code)]
-fn knights_tour_simple(pos: (usize, usize), step: usize, board: &mut ArrayBase<OwnedRepr<i8>, Ix2>, nr_fields: usize, solutions: &mut u64, nr_nodes: &mut u64, start: &Instant) {
+fn knights_tour_simple(pos: (usize, usize), step: usize, board: &mut ArrayBase<OwnedRepr<i8>, Ix2>, nr_fields: usize, target_pos: &(usize, usize), solutions: &mut u64, nr_nodes: &mut u64, start: &Instant) {
     *nr_nodes += 1;
     let backup = board[pos];
     board[pos] = -(step as i8);
-    if step == nr_fields /*&& pos == (2,2)*/ {
+    if step == nr_fields && pos == *target_pos {
         if *solutions & 0x3FFF == 0 { //16k
             let elapsed = start.elapsed().as_secs_f32();
             println!("{:6} Solutions in {:8.3}s {:8.2} Solutions/s {:13} Nodes", *solutions + 1, elapsed, (*solutions + 1) as f32/elapsed, nr_nodes);
@@ -20,50 +20,84 @@ fn knights_tour_simple(pos: (usize, usize), step: usize, board: &mut ArrayBase<O
         for delta in KNIGHT_MOVES {
             let new_pos = ((pos.0 as i8 + delta.0) as usize, (pos.1 as i8 + delta.1) as usize);
             if board[new_pos] > 0 {
-                knights_tour_simple(new_pos, step+1, board, nr_fields, solutions, nr_nodes, start);
+                knights_tour_simple(new_pos, step+1, board, nr_fields, target_pos, solutions, nr_nodes, start);
             }
         }
     }
     board[pos] = backup;
 }
 
-fn knights_tour(pos: (usize, usize), step: usize, board: &mut ArrayBase<OwnedRepr<i8>, Ix2>, nr_fields: usize, solutions: &mut u64, nr_nodes: &mut u64, start: &Instant) {
+fn knights_tour(pos: (usize, usize), step: usize, board: &mut ArrayBase<OwnedRepr<i8>, Ix2>, nr_fields: usize, target_pos: &(usize, usize), solutions: &mut u64, nr_nodes: &mut u64, start: &Instant) {
     *nr_nodes += 1;
     let backup = board[pos];
     board[pos] = -(step as i8);
-    if step ==nr_fields && pos == (2,2) {
+    if step ==nr_fields && pos == *target_pos {
         if *solutions & 0x7FFF == 0 { //16k
             let elapsed = start.elapsed().as_secs_f32();
             eprintln!("{:6} Solutions in {:8.3}s {:8.2} Solutions/s {:13} Nodes", *solutions + 1, elapsed, (*solutions + 1) as f32/elapsed, nr_nodes);
         }
         *solutions += 1;
     } else {
-        let mut moves = [((0,0),0);8];
-        for (i,delta) in KNIGHT_MOVES.iter().enumerate() {
+        let mut moves = [(0, (0,0));7];
+        let mut i = 0;
+        for delta in KNIGHT_MOVES {
             let new_pos = ((pos.0 as i8 + delta.0) as usize, (pos.1 as i8 + delta.1) as usize);
-            board[new_pos] -= 1;
-            moves[i] = (new_pos, board[new_pos]);
+            let reachable = board[new_pos];
+            if reachable > 0 {
+                board[new_pos] -= 1;
+                moves[i] = (board[new_pos], new_pos);
+                i += 1;
+            }
         };
 
-        moves.sort_by(|a,b| {
-            a.1.cmp(&b.1)
-        });
+        build_heap(&mut moves, i);
 
-        for (new_pos, nr_reachable) in moves.iter() {
-            if *nr_reachable == 0 {
-                break;
-            }
-            if *nr_reachable > 0 {
-                knights_tour(*new_pos, step+1, board, nr_fields, solutions, nr_nodes, start);
-            }
+        while i > 0 && moves[0].0 > 0 {
+            knights_tour(moves[0].1, step+1, board, nr_fields, target_pos, solutions, nr_nodes, start);
+            remove_min_from_heap(&mut moves, &mut i);
         }
 
         for delta in KNIGHT_MOVES {
             let new_pos = ((pos.0 as i8 + delta.0) as usize, (pos.1 as i8 + delta.1) as usize);
-            board[new_pos] +=1;
+            if board[new_pos] >= 0 {
+                board[new_pos] +=1;
+            }
         };
     }
     board[pos] = backup;
+}
+
+fn remove_min_from_heap<T: std::cmp::PartialOrd>(arr: &mut [T], n: &mut usize) {
+    *n -= 1;
+    arr.swap(0, *n );
+    if *n > 1 {
+        heapify(arr, *n, 0);
+    }
+}
+fn build_heap<T: std::cmp::PartialOrd>(arr: &mut [T], n: usize) {
+    if n > 1 {
+        for i in (0..n / 2).rev() {
+            heapify(arr, n, i);
+        }
+    }
+}
+fn heapify<T: std::cmp::PartialOrd>(arr: &mut [T], n: usize, mut i: usize) {
+    loop {
+        let mut smallest = i;
+        let l = 2 * i + 1;
+        let r = 2 * i + 2;
+        if l < n && arr[l] < arr[smallest] {
+            smallest = l;
+        }
+        if r < n && arr[r] < arr[smallest] {
+            smallest = r;
+        }
+        if smallest == i {
+            break;
+        }
+        arr.swap(i, smallest);
+        i = smallest;
+    }
 }
 
 fn main() {
@@ -127,21 +161,23 @@ fn main() {
 fn find_knight_tour_on(size_x: usize, size_y: usize) {
     let mut board = init_board(size_x, size_y);
 
-    let pos = (4usize, 3usize);
-    board[(2, 2)] = 10;
+    let target_pos = (size_x + 1, size_y + 1);
+    let pos = (target_pos.0 - 2, target_pos.1 - 1);
+
+    board[target_pos] = 10;
     //println!("{:?}", board);
     let mut solution = 0;
     let mut nr_nodes = 0;
 
     let start = Instant::now();
     eprintln!("Starting on {:2}x{:<2} ", size_x, size_y);
-    knights_tour(pos, 1, &mut board, size_y * size_x, &mut solution, &mut nr_nodes, &start);
+    knights_tour(pos, 1, &mut board, size_y * size_x, &target_pos, &mut solution, &mut nr_nodes, &start);
     let elapsed_time = start.elapsed().as_secs_f32();
     println!("On {:2}x{:<2} {:7} Solutions in {:8.3}s {:8.2} Solutions/s {:13} Nodes", size_x, size_y, solution, elapsed_time, solution as f32/elapsed_time, nr_nodes);
 }
 
 fn init_board(size_x: usize, size_y: usize) -> ArrayBase<OwnedRepr<i8>, Ix2> {
-    let mut board = ndarray::Array2::<i8>::from_elem((size_y + 4, size_x + 4), -99);
+    let mut board = ndarray::Array2::<i8>::from_elem((size_x + 4, size_y + 4), -99);
     for i in 2..board.nrows() - 2 {
         for j in 2..board.ncols() - 2 {
             board[[i, j]] = 0;
@@ -151,9 +187,45 @@ fn init_board(size_x: usize, size_y: usize) -> ArrayBase<OwnedRepr<i8>, Ix2> {
         for j in 2..board.ncols() - 2 {
             for delta in KNIGHT_MOVES {
                 let new_pos = ((i as i8 + delta.0) as usize, (j as i8 + delta.1) as usize);
-                board[new_pos] += 1;
+                if board[new_pos] >= 0 {
+                    board[new_pos] += 1;
+                }
             }
         }
     }
     board
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::rng;
+    use rand::seq::SliceRandom;
+
+    use super::*;
+
+    #[test]
+    fn bin_heap_test() {
+        for j in 1..20 {
+            let mut heap = (1..j).collect::<Vec<i8>>();
+            heap.shuffle(&mut rng());
+
+            let mut size = heap.len();
+
+            build_heap(&mut heap, size);
+
+            if j > 1 {
+                for i in 0..(heap.len() - 1) / 2 {
+                    assert!(heap[i] < heap[2 * i + 1]);
+                    if j > 2 {
+                        assert!(heap[i] < heap[2 * i + 2]);
+                    }
+                }
+            }
+
+        for i in 1..size + 1 {
+                assert_eq!(heap[0], i as i8);
+                remove_min_from_heap(&mut heap, &mut size);
+            }
+        }
+    }
 }
